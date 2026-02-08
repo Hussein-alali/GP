9da909beb2a789e43748a8f4fe0a642325acf24c
Taher mohamed <lTAHERl_2004@outlook.com>
2026-02-08 22:39:50 +0300

    First Part of home check

--- front/gpfront/components/Navbar.jsx
+++ front/gpfront/components/Navbar.jsx
@@ -1,3 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { Globe, Phone, Menu } from "lucide-react";
+
 export default function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <header className="header">
+      <div className="header-left">
+        <Phone size={18} />
+        <span>تسجيل الاهتمام</span>
+      </div>
+
+      <div className="header-center">
+        <span>English</span>
+        <Globe size={18} />
+      </div>
+
+      <div className="header-logo">
+        <h2>TMG Banān</h2>
+        <span>Al Riyadh</span>
+      </div>
+
+      <div className="header-right">
+        <span>القائمة</span>
+        <Menu size={22} />
+      </div>
+    </header>
+  );
 }
